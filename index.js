f9487d5ba91c435bdc16b68a315ed52a9466bf25
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-09 05:39:51 +0200

    add create method

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const btnEl = document.getElementById("btn");
 console.log(btnEl);
+const appEl = document.getElementById("app");
 
 function createNoteEl(id, content) {
-  console.log(id, content);
+  //console.log(id, content);
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Empty Note";
+  element.value = content;
+
+  element.addEventListener("dblclick", () => {
+    const warning = confirm("Are you sure you want to delete the note?");
+    if (warning) {
+      deleteNote(id, element);
+    }
+  });
+
+  element.addEventListener("input", () => {
+    updateNote(id, element.value);
+  });
+  return element;
 }
+
+function updateNote() {}
+
+function deleteNote() {}
 
 function addNote() {
   const noteObj = {
@@ -12,7 +33,8 @@ function addNote() {
   };
 
   //console.log(noteObj);
-  const NoteEl = createNoteEl(noteObj.id, noteObj.content);
+  const noteEl = createNoteEl(noteObj.id, noteObj.content);
+  appEl.insertBefore(noteEl, btnEl);
 }
 
 btnEl.addEventListener("click", addNote);
